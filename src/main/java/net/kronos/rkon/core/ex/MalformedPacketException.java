015a12f689f0368707449a95bb527bbb4486450f
StarWishSama <starwishsama@outlook.com>
2019-07-06 18:18:35 +0800

    (+) Reworked on RCon

---
+++ src/main/java/net/kronos/rkon/core/ex/MalformedPacketException.java
@@ -0,0 +1,11 @@
+package net.kronos.rkon.core.ex;
+
+import java.io.IOException;
+
+public class MalformedPacketException extends IOException {
+	
+	public MalformedPacketException(String message) {
+		super(message);
+	}
+	
+}
