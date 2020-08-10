2944190862c856ec063d0a98419ba9c4be631599
StarWishsama <starwishsama@outlook.com>
2020-08-10 20:47:31 +0800

    opti: 优化了石头剪刀布

--- src/main/kotlin/io/github/starwishsama/comet/commands/subcommands/chats/RSPCommand.kt
+++ src/main/kotlin/io/github/starwishsama/comet/commands/subcommands/chats/RSPCommand.kt
@@ -3,8 +3,11 @@ package io.github.starwishsama.comet.commands.subcommands.chats
 import cn.hutool.core.util.RandomUtil
 import io.github.starwishsama.comet.commands.CommandProps
 import io.github.starwishsama.comet.commands.interfaces.ChatCommand
+import io.github.starwishsama.comet.commands.interfaces.SuspendCommand
 import io.github.starwishsama.comet.enums.UserLevel
 import io.github.starwishsama.comet.objects.BotUser
+import io.github.starwishsama.comet.sessions.Session
+import io.github.starwishsama.comet.sessions.SessionManager
 import io.github.starwishsama.comet.utils.BotUtil
 import io.github.starwishsama.comet.utils.FileUtil
 import kotlinx.coroutines.delay
@@ -14,27 +17,12 @@ import net.mamoe.mirai.message.data.MessageChain
 import net.mamoe.mirai.message.uploadAsImage
 import java.io.File
 
-class RSPCommand : ChatCommand {
+class RSPCommand : ChatCommand, SuspendCommand {
     override suspend fun execute(event: MessageEvent, args: List<String>, user: BotUser): MessageChain {
         if (BotUtil.isNoCoolDown(event.sender.id)) {
             if (args.isNotEmpty()) {
-                val player = RockPaperScissors.getType(args[0])
-                return if (player != null) {
-                    val systemInt = RandomUtil.randomInt(RockPaperScissors.values().size)
-                    val system = RockPaperScissors.values()[systemInt]
-                    event.reply("角卷猜拳... 开始!")
-                    val img = File(FileUtil.getChildFolder("img"), system.fileName).uploadAsImage(event.subject)
-                    event.reply(img)
-                    delay(2_000)
-                    return when (RockPaperScissors.isWin(player, system)) {
-                        -1 -> BotUtil.sendMessage("平局! わため出的是${system.cnName}")
-                        0 -> BotUtil.sendMessage("你输了! わため出的是${system.cnName}")
-                        1 -> BotUtil.sendMessage("你赢了! わため出的是${system.cnName}")
-                        else -> BotUtil.sendMessage("这合理吗?")
-                    }
-                } else {
-                    BotUtil.sendMessage("你的拳法杂乱无章, 这合理吗?")
-                }
+                event.reply("角卷猜拳... 开始! 你要出什么呢?")
+                SessionManager.addSession(Session(user.id, this))
             } else {
                 return BotUtil.sendMessage(getHelp())
             }
@@ -46,14 +34,33 @@ class RSPCommand : ChatCommand {
 
     override fun getHelp(): String = "/cq [石头/剪刀/布] 石头剪刀布"
 
-    enum class RockPaperScissors(val cnName: String, val fileName: String) {
-        ROCK("石头", "rock.png"), SCISSORS("剪刀", "scissor.png"), PAPER("布", "paper.png");
+    override suspend fun handleInput(event: MessageEvent, user: BotUser, session: Session) {
+        val player = RockPaperScissors.getType(event.message.contentToString())
+        if (player != null) {
+            val systemInt = RandomUtil.randomInt(RockPaperScissors.values().size)
+            val system = RockPaperScissors.values()[systemInt]
+            delay(3_000)
+            val img = File(FileUtil.getChildFolder("img"), system.fileName).uploadAsImage(event.subject)
+            event.reply(img)
+            when (RockPaperScissors.isWin(player, system)) {
+                -1 -> event.reply(BotUtil.sendMessage("平局! わため出的是${system.cnName}"))
+                0 -> event.reply(BotUtil.sendMessage("你输了! わため出的是${system.cnName}"))
+                1 -> BotUtil.sendMessage("你赢了! わため出的是${system.cnName}")
+                else -> BotUtil.sendMessage("这合理吗?")
+            }
+        } else {
+            event.reply(BotUtil.sendMessage("你的拳法杂乱无章, 这合理吗?"))
+        }
+    }
+
+    enum class RockPaperScissors(val cnName: Array<String>, val fileName: String) {
+        ROCK(arrayOf("石头", "石子", "拳头", "拳"), "rock.png"), SCISSORS(arrayOf("剪刀"), "scissor.png"), PAPER(arrayOf("布", "包布"), "paper.png");
 
         companion object {
             fun getType(name: String): RockPaperScissors? {
                 values().forEach {
-                    if (it.cnName == name) {
-                        return it
+                    for (s in it.cnName) {
+                        if (s == name) return it
                     }
                 }
                 return null
