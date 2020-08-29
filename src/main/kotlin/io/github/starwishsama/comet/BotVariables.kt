c1b6ed647f72559cc5a62408b6d247281b9245df
StarWishsama <starwishsama@outlook.com>
2020-08-29 13:49:22 +0800

    chore: actions stuff

--- src/main/kotlin/io/github/starwishsama/comet/BotVariables.kt
+++ src/main/kotlin/io/github/starwishsama/comet/BotVariables.kt
@@ -30,7 +30,7 @@ import java.util.concurrent.ScheduledExecutorService
 
 object BotVariables {
     lateinit var filePath: File
-    const val version = "0.5.2-DEV-7b49b9f-20200829"
+    const val version = "0.5.2-DEV-8ef7d58-20200829"
     lateinit var bot: Bot
 
     fun isBotInitialized(): Boolean = ::bot.isInitialized
