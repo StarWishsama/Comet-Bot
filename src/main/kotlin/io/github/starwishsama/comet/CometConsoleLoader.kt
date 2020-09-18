c398219b28ad15346c14bfd779e95b442a0839b9
StarWishsama <starwishsama@outlook.com>
2020-09-18 20:03:12 +0800

    feat: 添加实验性 Console 插件版本

--- src/main/kotlin/io/github/starwishsama/comet/CometConsoleLoader.kt
+++ src/main/kotlin/io/github/starwishsama/comet/CometConsoleLoader.kt
@@ -1,10 +1,12 @@
 package io.github.starwishsama.comet
 
+import com.google.auto.service.AutoService
 import io.github.starwishsama.comet.commands.CommandExecutor
 import io.github.starwishsama.comet.file.DataSetup
 import io.github.starwishsama.comet.listeners.ConvertLightAppListener
 import io.github.starwishsama.comet.listeners.RepeatListener
 import net.mamoe.mirai.Bot
+import net.mamoe.mirai.console.plugin.jvm.JvmPlugin
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.contact.isBotMuted
@@ -19,6 +21,7 @@ import kotlin.time.ExperimentalTime
  * 注意: 使用 Console 启动是实验性功能!
  * 注意: 由于 Comet 的设计缺陷, 在 Console 有多个机器人的情况下可能无法正常工作.
  */
+@AutoService(JvmPlugin::class)
 object CometConsoleLoader : KotlinPlugin(
         JvmPluginDescription(
                 id = "io.github.starwishsama.comet",
@@ -49,10 +52,7 @@ object CometConsoleLoader : KotlinPlugin(
             /** 监听器 */
             val listeners = arrayOf(ConvertLightAppListener, RepeatListener)
 
-            listeners.forEach {
-                it.register(bot)
-                logger.info("[监听器] 已注册 ${it.getName()} 监听器")
-            }
+            listeners.forEach { it.register(bot) }
         }
     }
 
