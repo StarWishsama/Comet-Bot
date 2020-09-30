a3ac6a2d10e1a3d0b21ffe25f370de1632316416
StarWishsama <starwishsama@outlook.com>
2020-09-30 20:52:32 +0800

    chore: 优化蓝鸟点赞个数显示

--- src/main/kotlin/io/github/starwishsama/comet/utils/NumberUtil.kt
+++ src/main/kotlin/io/github/starwishsama/comet/utils/NumberUtil.kt
@@ -1,14 +1,10 @@
 package io.github.starwishsama.comet.utils
 
 object NumberUtil {
-    fun Number?.getBetterNumber(): String {
-        if (this == null) return "Unknown"
-        if (this !is Int || this !is Long) return toString()
-
-        val length = toString().length
+    fun Long.getBetterNumber(): String {
         return when {
-            length == 4 -> div(1000.0).toString() + "k"
-            length >= 5 -> div(10000.0).toString() + "w"
+            this >= 1000 -> div(1000.0).toString() + "k"
+            this >= 10000 -> div(10000.0).toString() + "w"
             else -> toString()
         }
     }
