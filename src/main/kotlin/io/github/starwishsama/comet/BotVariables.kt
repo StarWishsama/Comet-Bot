151b0d28466910f352fb4b0f8ca62706cd82391d
StarWishsama <starwishsama@outlook.com>
2020-12-06 00:55:40 +0800

    Switch back branch

--- src/main/kotlin/io/github/starwishsama/comet/BotVariables.kt
+++ src/main/kotlin/io/github/starwishsama/comet/BotVariables.kt
@@ -31,8 +31,8 @@ import java.util.concurrent.ScheduledExecutorService
 
 object BotVariables {
     lateinit var filePath: File
-    const val version = "0.6-M2-f92e4e9"
-    const val buildTime = "2020/12/06 00:52:53"
+    const val version = "0.6-M2-f45ab2e"
+    const val buildTime = "2020/12/06 00:55:27"
 
     /** 作为独立运行时使用的变量, 除 [Comet] 外禁止调用 */
     lateinit var bot: Bot
--- src/main/kotlin/io/github/starwishsama/comet/CometConsoleLoader.kt
+++ src/main/kotlin/io/github/starwishsama/comet/CometConsoleLoader.kt
@@ -1,18 +1,10 @@
 package io.github.starwishsama.comet
 
-import com.google.auto.service.AutoService
-import net.mamoe.mirai.Bot
-import net.mamoe.mirai.console.plugin.jvm.JvmPlugin
-import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
-import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
-import kotlin.time.ExperimentalTime
-
 /**
  * Comet 的 Console 插件部分
  *
  * 注意: 使用 Console 启动是实验性功能!
  * 由于 Comet 的设计缺陷, 在 Console 有多个机器人的情况下可能无法正常工作.
- */
 @AutoService(JvmPlugin::class)
 object CometConsoleLoader : KotlinPlugin(
     JvmPluginDescription(
@@ -41,4 +33,4 @@ object CometConsoleLoader : KotlinPlugin(
     override fun onDisable() {
         invokeWhenClose()
     }
-}
+}*/
