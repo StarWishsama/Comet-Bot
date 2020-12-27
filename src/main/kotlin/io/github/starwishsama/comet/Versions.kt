7c46855e5251a6d2349a224fe8dc8d21ff14adba
StarWishsama <starwishsama@outlook.com>
2020-12-27 17:53:06 +0800

    depen: Updated mirai to 2.0-M2-dev-10

--- src/main/kotlin/io/github/starwishsama/comet/Versions.kt
+++ src/main/kotlin/io/github/starwishsama/comet/Versions.kt
@@ -1,7 +1,7 @@
 package io.github.starwishsama.comet
 
 object Versions {
-    const val version = "0.6-M2-dccf631"
-    const val buildTime = "2020/12/27 17:48:20"
-    const val miraiVersion = "2.0-M2-dev-4"
+    const val version = "0.6-M2-a26ed49"
+    const val buildTime = "2020/12/27 17:52:19"
+    const val miraiVersion = "2.0-M2-dev-10"
 }
