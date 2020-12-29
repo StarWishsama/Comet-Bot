33ff225b6a4e00d20697b254156ab201188c9beb
StarWishsama <starwishsama@outlook.com>
2020-12-29 13:11:33 +0800

    chore: use token

--- src/main/kotlin/io/github/starwishsama/comet/Versions.kt
+++ src/main/kotlin/io/github/starwishsama/comet/Versions.kt
@@ -1,9 +1,9 @@
 package io.github.starwishsama.comet
 
 object Versions {
-    const val version = "0.6-M3-6730b2a"
-    const val buildTime = "2020/12/29 12:58:05"
-    const val miraiVersion = "2.0-M2-dev-11"
+    const val version = "@VERSION@"
+    const val buildTime = "@BUILDTIME@"
+    const val miraiVersion = "@MIRAI@"
 }
 
 /**
