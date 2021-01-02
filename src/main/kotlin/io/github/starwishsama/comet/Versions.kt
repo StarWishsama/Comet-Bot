99c9a1a49c46e42a0481325a3f81b659218a7bb4
StarWishsama <starwishsama@outlook.com>
2021-01-02 12:51:25 +0800

    why i modified versions

--- src/main/kotlin/io/github/starwishsama/comet/Versions.kt
+++ src/main/kotlin/io/github/starwishsama/comet/Versions.kt
@@ -1,9 +1,9 @@
 package io.github.starwishsama.comet
 
 object Versions {
-    const val version = "0.6-M3-d65e3d9"
-    const val buildTime = "2021/01/01 23:03:22"
-    const val miraiVersion = "2.0-M2-2"
+    const val version = "@VERSION@"
+    const val buildTime = "@BUILDTIME@"
+    const val miraiVersion = "@MIRAI@"
 }
 
 /**
