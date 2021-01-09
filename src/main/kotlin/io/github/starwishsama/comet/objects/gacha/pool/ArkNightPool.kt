1377096ca190773175c3da64197bd9b9adceda11
StarWishsama <starwishsama@outlook.com>
2021-01-09 12:54:56 +0800

    fix: 修复明日方舟错误的星级显示

--- src/main/kotlin/io/github/starwishsama/comet/objects/gacha/pool/ArkNightPool.kt
+++ src/main/kotlin/io/github/starwishsama/comet/objects/gacha/pool/ArkNightPool.kt
@@ -106,12 +106,12 @@ class ArkNightPool(override val name: String = "标准寻访" ) : GachaPool() {
             when (drawResult.size) {
                 1 -> {
                     val (name, _, rare) = drawResult[0]
-                    return "单次寻访结果\n$name ${DrawUtil.getStar(rare)}"
+                    return "单次寻访结果\n$name ${DrawUtil.getStar(rare, true)}"
                 }
                 10 -> {
                     return StringBuilder("十连寻访结果:\n").apply {
                         for ((name, _, rare) in drawResult) {
-                            append(name).append(" ").append(DrawUtil.getStar(rare)).append(" ")
+                            append(name).append(" ").append(DrawUtil.getStar(rare, true)).append(" ")
                         }
                     }.trim().toString()
                 }
