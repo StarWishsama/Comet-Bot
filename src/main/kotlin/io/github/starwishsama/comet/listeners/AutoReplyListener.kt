3edec47e661028030d3039ccf3c7c6958b54a64d
StarWishsama <starwishsama@outlook.com>
2021-01-11 13:46:34 +0800

    fix: 临时修复自动回复监听报错

--- src/main/kotlin/io/github/starwishsama/comet/listeners/AutoReplyListener.kt
+++ src/main/kotlin/io/github/starwishsama/comet/listeners/AutoReplyListener.kt
@@ -9,25 +9,29 @@ object AutoReplyListener : NListener {
     override fun register(bot: Bot) {
         bot.eventChannel.subscribeGroupMessages {
             always {
-                val cfg = GroupConfigManager.getConfig(group.id) ?: return@always
+                try {
+                    val cfg = GroupConfigManager.getConfigOrNew(group.id)
 
-                if (cfg.keyWordReply.isEmpty()) return@always
+                    if (cfg.keyWordReply.isEmpty()) return@always
 
-                val messageContent = message.contentToString()
-                cfg.keyWordReply.forEach {
-                    it.keyWords.forEach { keyWord ->
-                        if (messageContent.contains(keyWord)) {
-                            subject.sendMessage(message.quote() + it.reply.toMessageChain(subject))
-                            return@always
+                    val messageContent = message.contentToString()
+                    cfg.keyWordReply.forEach {
+                        it.keyWords.forEach { keyWord ->
+                            if (messageContent.contains(keyWord)) {
+                                subject.sendMessage(message.quote() + it.reply.toMessageChain(subject))
+                                return@always
+                            }
+                        }
+
+                        it.pattern.forEach { pattern ->
+                            if (pattern.matcher(messageContent).find()) {
+                                subject.sendMessage(message.quote() + it.reply.toMessageChain(subject))
+                                return@always
+                            }
                         }
                     }
-
-                    it.pattern.forEach { pattern ->
-                        if (pattern.matcher(messageContent).find()) {
-                            subject.sendMessage(message.quote() + it.reply.toMessageChain(subject))
-                            return@always
-                        }
-                    }
+                } catch (e: NullPointerException) {
+                    return@always
                 }
             }
         }
