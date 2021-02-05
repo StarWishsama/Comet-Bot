fe881c28ad48b40b9da0827ba87f93ebca6e8412
StarWishsama <starwishsama@outlook.com>
2021-02-05 12:30:58 +0800

    fix: 错误的时间单位

--- src/main/kotlin/io/github/starwishsama/comet/service/pusher/CometPusher.kt
+++ src/main/kotlin/io/github/starwishsama/comet/service/pusher/CometPusher.kt
@@ -66,7 +66,7 @@ abstract class CometPusher(val bot: Bot, val name: String) {
     }
 
     fun start() {
-        val f = TaskUtil.runScheduleTaskAsync(config.interval, config.interval, TimeUnit.SECONDS) {
+        val f = TaskUtil.runScheduleTaskAsync(config.interval, config.interval, TimeUnit.MILLISECONDS) {
             execute()
         }
         daemonLogger.info("$name 推送器已启动, $f")
