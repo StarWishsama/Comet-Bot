62d1954cd699dc45f60c7711a6ea5a5b82baad63
StarWishsama <starwishsama@outlook.com>
2021-04-26 13:07:52 +0800

    chore: 移除调试用途代码

--- src/main/kotlin/io/github/starwishsama/comet/api/thirdparty/github/GithubApi.kt
+++ src/main/kotlin/io/github/starwishsama/comet/api/thirdparty/github/GithubApi.kt
@@ -17,7 +17,6 @@ object GithubApi {
         return try {
             mapper.readValue<RepoInfo>(response)
         } catch (e: Exception) {
-            e.printStackTrace()
             null
         }
     }
