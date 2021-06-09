4c2393e25e21e3059800c16f88127c8735875450
StarWishsama <starwishsama@outlook.com>
2021-06-09 17:38:54 +0800

    refactor(command): AdminCommand

--- src/main/kotlin/io/github/starwishsama/comet/commands/chats/AdminCommand.kt
+++ src/main/kotlin/io/github/starwishsama/comet/commands/chats/AdminCommand.kt
@@ -11,19 +11,18 @@
 package io.github.starwishsama.comet.commands.chats
 
 
-import io.github.starwishsama.comet.api.command.CommandExecutor
 import io.github.starwishsama.comet.api.command.CommandProps
 import io.github.starwishsama.comet.api.command.interfaces.ChatCommand
 import io.github.starwishsama.comet.api.command.interfaces.UnDisableableCommand
 import io.github.starwishsama.comet.enums.UserLevel
 import io.github.starwishsama.comet.objects.BotUser
-import io.github.starwishsama.comet.utils.CometUtil
+import io.github.starwishsama.comet.service.command.AdminService.addPermission
+import io.github.starwishsama.comet.service.command.AdminService.giveCommandTime
+import io.github.starwishsama.comet.service.command.AdminService.listPermissions
 import io.github.starwishsama.comet.utils.CometUtil.toChain
-import io.github.starwishsama.comet.utils.StringUtil.convertToChain
 import net.mamoe.mirai.contact.MemberPermission
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.events.MessageEvent
-import net.mamoe.mirai.message.data.EmptyMessageChain
 import net.mamoe.mirai.message.data.MessageChain
 
 
@@ -31,13 +30,13 @@ import net.mamoe.mirai.message.data.MessageChain
 class AdminCommand : ChatCommand, UnDisableableCommand {
     override suspend fun execute(event: MessageEvent, args: List<String>, user: BotUser): MessageChain {
         return if (args.isEmpty()) {
-            "命令不存在, 使用 /admin help 查看更多".toChain()
+            getHelp().toChain()
         } else {
             when (args[0]) {
-                "help", "帮助" -> getHelp().convertToChain()
-                "permlist", "权限列表", "qxlb" -> permList(user, args, event)
-                "permadd", "添加权限", "tjqx" -> permAdd(user, args, event)
-                "give", "增加次数" -> giveCommandUseTime(event, args)
+                "help", "帮助" -> getHelp().toChain()
+                "permlist", "权限列表", "qxlb" -> listPermissions(user, args, event)
+                "permadd", "添加权限", "tjqx" -> addPermission(user, args, event)
+                "give", "增加次数" -> giveCommandTime(event, args)
                 else -> "命令不存在, 使用 /admin help 查看更多".toChain()
             }
         }
@@ -48,8 +47,8 @@ class AdminCommand : ChatCommand, UnDisableableCommand {
 
     override fun getHelp(): String = """
         ======= 命令帮助 =======
-        /admin dk (开始时间) [结束时间] 创建一个打卡
-        /admin dksj 查看最近一次打卡的数据
+        /admin help 展示此帮助列表
+        /admin permlist [用户] 查看用户拥有的权限
         /admin permadd [用户] [权限名] 给一个用户添加权限
         /admin give [用户] [命令条数] 给一个用户添加命令条数
     """.trimIndent()
@@ -59,59 +58,5 @@ class AdminCommand : ChatCommand, UnDisableableCommand {
         if (user.compareLevel(bLevel)) return true
         if (e is GroupMessageEvent && e.sender.permission != MemberPermission.MEMBER) return true
         return false
-    }
-
-    private fun permList(user: BotUser, args: List<String>, event: MessageEvent): MessageChain {
-        return if (args.size > 1) {
-            val target: BotUser? = CometUtil.parseAtAsBotUser(event, args[1])
-            val permission = target?.getPermissions()
-            if (permission != null) {
-                toChain(permission)
-            } else {
-                toChain("该用户没有任何权限")
-            }
-        } else {
-            toChain(user.getPermissions())
-        }
-    }
-
-    private fun permAdd(user: BotUser, args: List<String>, event: MessageEvent): MessageChain {
-        if (user.isBotOwner()) {
-            if (args.size > 1) {
-                val target: BotUser? = CometUtil.parseAtAsBotUser(event, args[1])
-
-                val validate =
-                    CommandExecutor.getCommands().parallelStream().filter { it.getProps().permission == args[2] }
-                        .findAny().isPresent
-
-                return if (validate) {
-                    target?.addPermission(args[2])
-                    toChain("添加权限成功")
-                } else {
-                    "找不到 ${args[2]} 对应的命令".toChain()
-                }
-            }
-        } else {
-            return toChain("你没有权限")
-        }
-        return EmptyMessageChain
-    }
-
-    private fun giveCommandUseTime(event: MessageEvent, args: List<String>): MessageChain {
-        if (args.size > 1) {
-            val target: BotUser? = CometUtil.parseAtAsBotUser(event, args[1])
-
-            return if (target != null) {
-                if (args[2].toInt() <= 1000000) {
-                    target.addTime(args[2].toInt())
-                    toChain("成功为 $target 添加 ${args[2]} 次命令条数")
-                } else {
-                    toChain("给予的次数超过系统限制上限")
-                }
-            } else {
-                toChain("找不到此用户")
-            }
-        }
-        return EmptyMessageChain
     }
 }
