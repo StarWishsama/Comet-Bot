77a754c98d9185dd40b21e4fb16c4bb1be437026
StarWishsama <starwishsama@outlook.com>
2021-06-14 17:43:03 +0800

    docs(GitHub): 添加文档

--- src/main/kotlin/io/github/starwishsama/comet/objects/config/GithubRepos.kt
+++ src/main/kotlin/io/github/starwishsama/comet/objects/config/GithubRepos.kt
@@ -113,14 +113,29 @@ data class GithubRepos(
 
 enum class SecretStatus {
 
+    /**
+     * 找不到任何使用 Secret 的项目, 可视作成功
+     */
     NO_SECRET,
 
+    /**
+     * 已找到对应 Secret 的项目, 可视作成功
+     */
     HAS_SECRET,
 
+    /**
+     * 已找到对应 Secret 的项目, 但无法匹配
+     */
     UNAUTHORIZED,
 
+    /**
+     * 找不到请求的项目
+     */
     NOT_FOUND,
 
+    /**
+     * 因其他原因验证失败
+     */
     FAILED
 }
 
