39ac9adfef94a06061fb6bfda94174b670dee321
StarWishsama <starwishsama@outlook.com>
2021-11-27 21:29:24 +0800

    feat(wrapper): add audio wrapper

--- src/main/kotlin/io/github/starwishsama/comet/objects/wrapper/MessageElements.kt
+++ src/main/kotlin/io/github/starwishsama/comet/objects/wrapper/MessageElements.kt
@@ -20,8 +20,10 @@ import io.github.starwishsama.comet.utils.serialize.WrapperConverter
 import io.github.starwishsama.comet.utils.uploadAsImage
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
+import net.mamoe.mirai.contact.AudioSupported
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.message.data.*
+import net.mamoe.mirai.utils.ExternalResource.Companion.toExternalResource
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
 import net.mamoe.mirai.utils.MiraiExperimentalApi
 import java.io.File
@@ -143,4 +145,26 @@ data class XmlElement(val content: String) : WrapperElement {
 
     override fun asString(): String = "XML 消息"
 
+}
+
+data class Voice(val filePath: String) : WrapperElement {
+    override val className: String = this::class.java.name
+
+    override fun toMessageContent(subject: Contact?): MessageContent {
+        requireNotNull(subject) { "subject cannot be null!" }
+
+        if (subject !is AudioSupported) {
+            return PlainText("语音消息只能发送给好友或群")
+        }
+
+        if (filePath.isNotEmpty() && File(filePath).exists()) {
+            return runBlocking {
+                subject.uploadAudio(File(filePath).toExternalResource())
+            }
+        }
+
+        throw RuntimeException("Unable to convert Voice to MessageChain, Raw path: $this")
+    }
+
+    override fun asString(): String = "语音消息"
 }
