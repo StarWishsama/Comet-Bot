c2d2f4830c73db5af3981e13ab697b1e5b29623a
StarWishsama <starwishsama@outlook.com>
2022-01-16 14:05:36 +0800

    :construction_worker: chore: use gradle kts

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019-2022 StarWishsama.
+ *
+ * 此源代码的使用受 GNU General Affero Public License v3.0 许可证约束, 欲阅读此许可证, 可在以下链接查看.
+ *  Use of this source code is governed by the GNU AGPLv3 license which can be found through the following link.
+ *
+ * https://github.com/StarWishsama/Comet-Bot/blob/master/LICENSE
+ *
+ */
+
+object Versions {
+    const val miraiVersion = "2.9.2"
+    const val jacksonVersion = "2.13.1"
+    const val hutoolVersion = "5.7.19"
+    const val yamlktVersion = "0.10.2"
+    const val ktorVersion = "1.5.4"
+}
