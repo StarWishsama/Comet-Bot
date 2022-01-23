9eacb52b7d75775aac7a7fe779c5a9602aaae1ca
StarWishsama <starwishsama@outlook.com>
2022-01-23 13:31:44 +0800

    :speech_balloon: refactor(noabbr): improve abbr search result

--- src/main/kotlin/io/github/starwishsama/comet/objects/tasks/network/impl/NoAbbrRequestTask.kt
+++ src/main/kotlin/io/github/starwishsama/comet/objects/tasks/network/impl/NoAbbrRequestTask.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021 StarWishsama.
+ * Copyright (c) 2019-2022 StarWishsama.
  *
  * 此源代码的使用受 GNU General Affero Public License v3.0 许可证约束, 欲阅读此许可证, 可在以下链接查看.
  *  Use of this source code is governed by the GNU AGPLv3 license which can be found through the following link.
@@ -35,7 +35,7 @@ class NoAbbrRequestTask(override val content: Contact, override val param: Strin
             if (result is AbbrSearchResponse && !result.isEmpty()) {
                 content.sendMessage(result.toMessageWrapper().toMessageChain(content))
             } else {
-                content.sendMessage(LocalizationManager.getLocalizationText("network-error").toChain())
+                content.sendMessage(LocalizationManager.getLocalizationText("message.network-error").toChain())
             }
         }
     }
