880946cd56e54aa17d04782e5befb31ad4c5ae6b
StarWishsama <starwishsama@outlook.com>
2022-02-27 13:20:40 +0800

    🚀 chore: fix script

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@
  *
  */
 
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -145,8 +144,7 @@ tasks.shadowJar {
     println("Comet >> Now building Comet ${project.version}...")
 }
 
-tasks.withType<KotlinCompile> {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
     kotlinOptions.freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
 }
-val compileKotlin: KotlinCompile by tasks
