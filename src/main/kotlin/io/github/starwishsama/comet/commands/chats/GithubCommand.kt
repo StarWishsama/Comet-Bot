12e17d7e24b6ab644d38b4fb22b0bb0b9e8862ef
StarWishsama <starwishsama@outlook.com>
2022-04-11 23:09:05 +0800

    :bug: fix(github): fix filter

--- src/main/kotlin/io/github/starwishsama/comet/commands/chats/GithubCommand.kt
+++ src/main/kotlin/io/github/starwishsama/comet/commands/chats/GithubCommand.kt
@@ -41,7 +41,7 @@ object GithubCommand : ChatCommand, ConversationCommand {
             "lookup", "cx" -> lookupRepo(args, event)
             "filter" -> {
                 if (args.size == 3) {
-                    when (args[2]) {
+                    when (args[1]) {
                         "add", "tj" -> addBranchFilter(args, event, user)
                         "remove", "rm", "sc" -> removeBranchFilter(args, event, user)
                         else -> getHelp().convertToChain()
