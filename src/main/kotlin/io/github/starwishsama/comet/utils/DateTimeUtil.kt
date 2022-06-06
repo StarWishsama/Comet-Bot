de390afa90900f45bb6cdec4b9a1256aad9065ba
StarWishsama <starwishsama@outlook.com>
2022-06-06 20:20:35 +0800

    :bug: fix(gaokao): use UTC time

--- src/main/kotlin/io/github/starwishsama/comet/utils/DateTimeUtil.kt
+++ src/main/kotlin/io/github/starwishsama/comet/utils/DateTimeUtil.kt
@@ -1,10 +1,11 @@
 package io.github.starwishsama.comet.utils
 
 import java.time.LocalDateTime
+import java.time.ZoneId
 
 internal val gaokaoDateTime = run {
-    val now = LocalDateTime.now()
-    val thisYear = LocalDateTime.of(now.year, 6, 7, 0, 0)
-    val nextYear = LocalDateTime.of(now.year + 1, 6, 7, 0, 0)
+    val now = LocalDateTime.now().atZone(ZoneId.of("UTC"))
+    val thisYear = LocalDateTime.of(now.year, 6, 7, 0, 0).atZone(ZoneId.of("UTC"))
+    val nextYear = LocalDateTime.of(now.year + 1, 6, 7, 0, 0).atZone(ZoneId.of("UTC"))
     if (now > thisYear) nextYear else thisYear
 }
