9765cea3d32890d8adda73cb89c8a1a15aa8aa46
StarWishsama <starwishsama@outlook.com>
2022-07-14 14:29:56 +0800

    :bug: fix: build

--- comet-api/src/test/kotlin/ren/natsuyuk1/comet/api/test/consts.kt
+++ comet-api/src/test/kotlin/ren/natsuyuk1/comet/api/test/consts.kt
@@ -3,6 +3,7 @@ package ren.natsuyuk1.comet.api.test
 import ren.natsuyuk1.comet.api.Comet
 import ren.natsuyuk1.comet.api.command.PlatformCommandSender
 import ren.natsuyuk1.comet.api.config.CometConfig
+import ren.natsuyuk1.comet.api.user.Group
 import ren.natsuyuk1.comet.utils.coroutine.ModuleScope
 import ren.natsuyuk1.comet.utils.message.MessageWrapper
 
@@ -14,6 +15,8 @@ val fakeComet = object : Comet(CometConfig, logger, ModuleScope("TestInstance"))
     override fun afterLogin() {}
 
     override fun close() {}
+
+    override fun getGroup(id: Long): Group? = null
 }
 
 val fakeSender = object : PlatformCommandSender() {
@@ -22,6 +25,7 @@ val fakeSender = object : PlatformCommandSender() {
     override val id: Long = 0
     override val name: String = "Dummy"
     override var card: String = "Dummy"
+    override val platformName: String = "mirai"
 
     override fun sendMessage(message: MessageWrapper) {
         logger.info { message.parseToString() }
