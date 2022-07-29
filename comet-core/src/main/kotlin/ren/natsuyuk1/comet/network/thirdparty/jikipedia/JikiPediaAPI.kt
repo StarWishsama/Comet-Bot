5852d00ab4963d60535b21bff721ef2b44e38922
StarWishsama <starwishsama@outlook.com>
2022-07-29 14:12:05 +0800

    :fire: chores: clean up code

--- comet-core/src/main/kotlin/ren/natsuyuk1/comet/network/thirdparty/jikipedia/JikiPediaAPI.kt
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/network/thirdparty/jikipedia/JikiPediaAPI.kt
@@ -1,34 +1,12 @@
 package ren.natsuyuk1.comet.network.thirdparty.jikipedia
 
-import cn.hutool.core.codec.Base64
-import cn.hutool.core.lang.UUID
 import io.ktor.client.request.*
 import ren.natsuyuk1.comet.api.config.CometConfig
 import ren.natsuyuk1.comet.consts.cometClient
-import java.security.MessageDigest
-import javax.crypto.Cipher
-import javax.crypto.spec.IvParameterSpec
-import javax.crypto.spec.SecretKeySpec
 
 private val logger = mu.KotlinLogging.logger {}
 
 object JikiPediaAPI {
-    private val iv = "12uh00]35#@(poj[".toByteArray()
-
-    private fun generateKey(): ByteArray {
-        val base = "web_2.7.3a_12uh00]35#@(poj[".toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        return md.digest(base)
-    }
-
-    fun encodeToJikiXID(): String {
-        val base = "jikipedia_xid_${UUID.randomUUID()}".toByteArray()
-        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-        cipher.init(Cipher.ENCRYPT_MODE, SecretKeySpec(generateKey(), "AES"), IvParameterSpec(ByteArray(16)))
-        val cipherText = cipher.doFinal(base)
-        return Base64.encode(cipherText)
-    }
-
     suspend fun search(keyword: String): JikiPediaSearchResult {
         return cometClient.client.post<JikiPediaSearchResult>("https://api.jikipedia.com/go/search_entities") {
             headers {
