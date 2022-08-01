452451fdffd316255d32096923f352860ff3b0fa
StarWishsama <starwishsama@outlook.com>
2022-08-01 18:52:13 +0800

    :fire: fix: fix license

--- comet-mirai-wrapper/src/main/kotlin/ren/natsuyuk1/comet/mirai/MiraiComet.kt
+++ comet-mirai-wrapper/src/main/kotlin/ren/natsuyuk1/comet/mirai/MiraiComet.kt
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2019-2022 StarWishsama.
- *
- * 此源代码的使用受 MIT 许可证约束, 欲阅读此许可证, 可在以下链接查看.
- * Use of this source code is governed by the MIT License which can be found through the following link.
- *
- * https://github.com/StarWishsama/Comet-Bot/blob/dev/LICENSE
- */
-
 package ren.natsuyuk1.comet.mirai
 
 import kotlinx.coroutines.launch
