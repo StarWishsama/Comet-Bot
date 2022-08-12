c38f49300bdee071fdd24527f17a1bb148a8d65b
StarWishsama <starwishsama@outlook.com>
2022-08-12 17:51:31 +0800

    :sparkles: feat(user): add user data migrator

--- comet-core/src/main/kotlin/ren/natsuyuk1/comet/migrator/olddata/OldCometUser.kt
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/migrator/olddata/OldCometUser.kt
@@ -1,12 +1,22 @@
 package ren.natsuyuk1.comet.migrator.olddata
 
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import ren.natsuyuk1.comet.api.user.UserLevel
+import ren.natsuyuk1.comet.utils.time.yyMMddWithTimePattern
 import java.time.LocalDateTime
 import java.util.*
 
+@Serializable
 data class OldCometUser(
     val id: Long,
+    @Serializable(UUIDSerializer::class)
     val uuid: UUID,
+    @Serializable(LocalDateTimeSerializer::class)
     var checkInDateTime: LocalDateTime = LocalDateTime.now().minusDays(1),
     var coin: Double = 0.0,
     var checkInCount: Int = 0,
@@ -17,6 +27,31 @@ data class OldCometUser(
     private val permissions: MutableSet<OldCometPermission> = mutableSetOf(),
 )
 
+object UUIDSerializer : KSerializer<UUID> {
+    override val descriptor = PrimitiveSerialDescriptor("uuid", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): UUID {
+        return UUID.fromString(decoder.decodeString())
+    }
+
+    override fun serialize(encoder: Encoder, value: UUID) {
+        encoder.encodeString(value.toString())
+    }
+}
+
+object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+    override val descriptor = PrimitiveSerialDescriptor("uuid", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        return LocalDateTime.parse(decoder.decodeString(), yyMMddWithTimePattern)
+    }
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        encoder.encodeString(yyMMddWithTimePattern.format(value))
+    }
+}
+
+@Serializable
 enum class OldUserLevel {
     USER, ADMIN, OWNER, CONSOLE;
 }
@@ -29,7 +64,7 @@ fun OldUserLevel.toUserLevel(): UserLevel =
         OldUserLevel.CONSOLE -> UserLevel.CONSOLE
     }
 
-
+@Serializable
 data class OldCometPermission(
     val name: String,
     val defaultLevel: OldUserLevel
