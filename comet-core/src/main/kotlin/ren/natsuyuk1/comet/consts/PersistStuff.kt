e3be5827b39c133423145dfecef4a0ae8edaa1f6
Colerar <233hbj@gmail.com>
2022-08-17 01:49:19 +0800

    feat: `NowCommand`

--- comet-core/src/main/kotlin/ren/natsuyuk1/comet/consts/PersistStuff.kt
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/consts/PersistStuff.kt
@@ -11,6 +11,7 @@ import ren.natsuyuk1.comet.api.user.UserTable
 import ren.natsuyuk1.comet.commands.*
 import ren.natsuyuk1.comet.objects.config.CometServerConfig
 import ren.natsuyuk1.comet.objects.github.data.GithubRepoData
+import ren.natsuyuk1.comet.objects.nowcmd.NowCmdConfigTable
 import ren.natsuyuk1.comet.objects.pjsk.ProjectSekaiDataTable
 import ren.natsuyuk1.comet.objects.pjsk.ProjectSekaiUserDataTable
 
@@ -29,6 +30,7 @@ val cometTables =
         UserPermissionTable,
         ProjectSekaiDataTable,
         ProjectSekaiUserDataTable,
+        NowCmdConfigTable
     )
 
 val defaultCommands: List<AbstractCommandNode<*>> =
@@ -86,5 +88,10 @@ val defaultCommands: List<AbstractCommandNode<*>> =
             listOf(BangumiCommand.Schedule.SCHEDULE, BangumiCommand.Search.SEARCH)
         ) { comet, sender, subject, wrapper, user ->
             BangumiCommand(comet, sender, subject, wrapper, user)
-        }
+        },
+        CommandNode(
+            NOW,
+        ) { comet, sender, subject, wrapper, user ->
+            NowCommand(comet, sender, subject, wrapper, user)
+        },
     )
---
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/objects/nowcmd/NowCmdConfigTable.kt
@@ -0,0 +1,96 @@
+package ren.natsuyuk1.comet.objects.nowcmd
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import org.jetbrains.exposed.sql.*
+import ren.natsuyuk1.comet.api.platform.LoginPlatform
+import java.util.*
+
+private typealias NTable = NowCmdConfigTable
+
+object NowCmdConfigTable : Table("now_cmd_config") {
+    private val platformType = enumerationByName<LoginPlatform>("type", 20)
+    private val isGroup = bool("is_group")
+    private val id = long("id")
+    private val config = text("config")
+
+    override val primaryKey = PrimaryKey(platformType, isGroup, id)
+
+    suspend fun setConfig(
+        platform: LoginPlatform,
+        isGroup: Boolean,
+        id: Long,
+        config: Config,
+    ): Unit = withContext(Dispatchers.IO) {
+        if (!hasConfig(platform, isGroup, id)) {
+            insert {
+                it[platformType] = platform
+                it[NTable.isGroup] = isGroup
+                it[NTable.id] = id
+                it[NTable.config] = Json.encodeToString(config)
+            }
+        } else {
+            update {
+                it[platformType] = platform
+                it[NTable.isGroup] = isGroup
+                it[NTable.id] = id
+                it[NTable.config] = Json.encodeToString(config)
+            }
+        }
+    }
+
+    suspend fun hasConfig(
+        platform: LoginPlatform,
+        isGroup: Boolean,
+        id: Long,
+    ): Boolean = withContext(Dispatchers.IO) {
+        select {
+            (platformType eq platform) and
+                (NTable.isGroup eq isGroup) and
+                (NTable.id eq id)
+        }.firstOrNull() != null
+    }
+
+    suspend fun getConfig(
+        platform: LoginPlatform,
+        isGroup: Boolean,
+        id: Long,
+    ): Config? = withContext(Dispatchers.IO) {
+        select {
+            (platformType eq platform) and
+                (NTable.isGroup eq isGroup) and
+                (NTable.id eq id)
+        }.map {
+            Json.decodeFromString<Config>(it[config])
+        }.firstOrNull()
+    }
+}
+
+@Serializable
+class Config(
+    val formatter: String? = null,
+
+    val timezones: List<Pair<@Serializable(TimeZoneSerializer::class) TimeZone, String?>>? = null,
+)
+
+object TimeZoneSerializer : KSerializer<TimeZone> {
+    override fun deserialize(decoder: Decoder): TimeZone {
+        return TimeZone.getTimeZone(decoder.decodeString())
+    }
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("TimeZone", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: TimeZone) {
+        encoder.encodeString(value.id)
+    }
+}
