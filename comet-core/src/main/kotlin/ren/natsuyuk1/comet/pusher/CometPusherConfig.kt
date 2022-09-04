1ea392be5605dde91b0e9085fde011c48360d309
StarWishsama <starwishsama@outlook.com>
2022-09-04 21:35:22 +0800

    :hammer: chores(pusher): fix typo

--- comet-core/src/main/kotlin/ren/natsuyuk1/comet/pusher/CometPusherConfig.kt
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/pusher/CometPusherConfig.kt
@@ -10,7 +10,7 @@ data class CometPusherConfig(
     val pushInterval: Int,
     @Comment("夜间模式推送周期, 默认为一小时, 单位见 pushIntervalUnit")
     val nightPushInterval: Int = 3600,
-    @Comment("推送器夜间模式时期, 格式为 [开始时]-[结束时], 例如 23-6 指今日 23 点到次日 6 点为夜间模式时期.")
+    @Comment("推送器夜间模式时间段, 格式为 [开始时]-[结束时], 例如 23-6 指今日 23 点到次日 6 点为夜间模式时间段.")
     var nightModeDuration: String = "0-6",
     @Comment("推送器的推送周期时间单位, 默认为秒.")
     val pushIntervalUnit: DurationUnit = DurationUnit.SECONDS,
