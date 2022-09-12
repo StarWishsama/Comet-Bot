fda14b163ac9c8f44ab164c7c8cc44e5d48bf649
StarWishsama <starwishsama@outlook.com>
2022-09-12 21:10:04 +0800

    :speech_balloon: improve(arcaea): show last play score and rating

--- comet-core/src/main/kotlin/ren/natsuyuk1/comet/network/thirdparty/arcaea/data/ArcaeaUserInfo.kt
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/network/thirdparty/arcaea/data/ArcaeaUserInfo.kt
@@ -53,7 +53,9 @@ data class ArcaeaUserInfo(
             @SerialName("clear_type")
             val clearType: Int,
             @SerialName("rating")
-            val rating: Double
+            val rating: Double,
+            @SerialName("constant")
+            val constant: String,
         )
     }
 
@@ -64,23 +66,28 @@ data class ArcaeaUserInfo(
         if (!data.recentPlayScore.isNullOrEmpty()) {
             appendLine()
             val lastPlay = data.recentPlayScore.first()
-            appendText("最近游玩 >> ${ArcaeaClient.getSongNameByID(lastPlay.songID)} (${lastPlay.difficulty.formatDifficulty()}) | ${lastPlay.clearType.formatType()}")
+            appendText("最近游玩 >>", true)
+            appendText("${ArcaeaClient.getSongNameByID(lastPlay.songID)} (${lastPlay.difficulty.formatDifficulty()})", true)
+            appendText("${lastPlay.score} [${lastPlay.score.formatScore()} | ${lastPlay.constant}] | ${lastPlay.clearType.formatType()}")
+            appendLine()
         }
 
         if (!data.ratingRecord.isEmpty()) {
             appendLine()
             val lastPtt = data.ratingRecord[data.ratingRecord.size - 2].jsonArray.last().jsonPrimitive.content.toDouble()
-            appendText("距上次查询 ptt 已变化 >> ${((data.rating - lastPtt) / 100.0).fixDisplay()}")
+            appendText("距上次游玩 ptt 已变化 ${((data.rating - lastPtt) / 100.0).fixDisplay()}")
         }
     }
 
     private fun Int.formatType(): String =
         when (this) {
             // 4 简单型角色, 5 困难型角色
-            0, 4, 5 -> "FAILED"
-            1 -> "TC"
-            2 -> "FR"
-            3 -> "PM"
+            0 -> "FAILED"
+            1 -> "Clear"
+            4 -> "Easy Clear"
+            5 -> "Hard Clear"
+            2 -> "Full Recall"
+            3 -> "Pure Memory"
             else -> "UNKNOWN ($this)"
         }
 
@@ -92,6 +99,19 @@ data class ArcaeaUserInfo(
             4 -> "BYD"
             else -> "Unknown ($this)"
         }
+
+    private fun Int.formatScore(): String =
+        when {
+            this in 8600000..8899999 -> "C"
+            this in 8900000..9199999 -> "B"
+            this in 9200000..9499999 -> "A"
+            this in 9500000..9799999 -> "AA"
+            this in 9800000..9899999 -> "EX"
+            this >= 9900000 -> "EX+"
+            this in 0..8599999 -> "D"
+            else -> "UNKNOWN"
+        }
+
 
     fun getActualPtt(): String = (data.rating / 100.0).fixDisplay()
 }
