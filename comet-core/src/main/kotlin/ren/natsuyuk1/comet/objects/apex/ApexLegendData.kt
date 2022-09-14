9c14e7b77de44dfb7b4c1dc711f14902b48d6ea0
StarWishsama <starwishsama@outlook.com>
2022-09-14 22:00:50 +0800

    :bug: fix(apex): DO NOT COPY AND PASTE

--- comet-core/src/main/kotlin/ren/natsuyuk1/comet/objects/apex/ApexLegendData.kt
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/objects/apex/ApexLegendData.kt
@@ -13,7 +13,7 @@ object ApexLegendDataTable : IdTable<UUID>("arcaea_user_data") {
     override val id: Column<EntityID<UUID>> = ApexLegendDataTable.uuid("user").entityId()
     override val primaryKey = PrimaryKey(id)
 
-    val userID = ren.natsuyuk1.comet.objects.arcaea.ArcaeaUserDataTable.varchar("user_id", 9)
+    val userID = varchar("user_id", 9)
 }
 
 class ApexLegendData(id: EntityID<UUID>) : UUIDEntity(id) {
