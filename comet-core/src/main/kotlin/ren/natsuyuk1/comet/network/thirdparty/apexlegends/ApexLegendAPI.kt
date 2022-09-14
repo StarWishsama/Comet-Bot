11ceabb866b56617b0a1c9e8d24b855f5d03069e
StarWishsama <starwishsama@outlook.com>
2022-09-14 22:10:14 +0800

    :bug: fix(apex): redundant `/`

--- comet-core/src/main/kotlin/ren/natsuyuk1/comet/network/thirdparty/apexlegends/ApexLegendAPI.kt
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/network/thirdparty/apexlegends/ApexLegendAPI.kt
@@ -8,7 +8,7 @@ import ren.natsuyuk1.comet.network.thirdparty.apexlegends.data.ApexIDInfo
 import ren.natsuyuk1.comet.network.thirdparty.apexlegends.data.ApexPlayerInfo
 
 object ApexLegendAPI {
-    private const val API_ROUTE = "https://api.mozambiquehe.re/"
+    private const val API_ROUTE = "https://api.mozambiquehe.re"
 
     suspend fun CometClient.fetchUserInfo(uid: String, platform: String): ApexPlayerInfo = client.get("$API_ROUTE/bridge") {
         parameter("uid", uid)
