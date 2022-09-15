569c9a56170ec0fbaf943957d9ca9670b137cec5
StarWishsama <starwishsama@outlook.com>
2022-09-15 21:40:10 +0800

    :test_tube: fix(ci): ignore ci

--- comet-core/src/test/kotlin/ren/natsuyuk1/comet/test/network/thirdparty/apex/TestApexLegends.kt
+++ comet-core/src/test/kotlin/ren/natsuyuk1/comet/test/network/thirdparty/apex/TestApexLegends.kt
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.TestInstance
 import ren.natsuyuk1.comet.api.config.CometGlobalConfig
 import ren.natsuyuk1.comet.consts.cometClient
 import ren.natsuyuk1.comet.network.thirdparty.apexlegends.ApexLegendAPI.fetchUserID
+import ren.natsuyuk1.comet.test.isCI
 import ren.natsuyuk1.comet.test.print
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestApexLegends {
     @BeforeAll
     fun init() {
+        if (isCI()) return
+
         runBlocking {
             CometGlobalConfig.init()
         }
@@ -20,6 +23,8 @@ class TestApexLegends {
 
     @Test
     fun testUsernameToUID() {
+        if (isCI()) return
+
         val username = "NaTsuYuk1_"
 
         runBlocking {
