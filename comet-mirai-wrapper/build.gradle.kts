1bd882f834d6693861bf5627aadd2c0962a3ac26
StarWishsama <starwishsama@outlook.com>
2022-10-08 00:59:12 +0800

    :bug: fix(mirai): fix mirai version code

--- comet-mirai-wrapper/build.gradle.kts
+++ comet-mirai-wrapper/build.gradle.kts
@@ -24,9 +24,9 @@ dependencies {
     compileOnly(project(":comet-core"))
     compileOnly(project(":comet-utils"))
 
-    implementation("net.mamoe:mirai-core:2.13.0-dev-394c0a5")
-    implementation("net.mamoe:mirai-core-api:2.13.0-dev-394c0a5")
-    implementation("net.mamoe:mirai-core-utils:2.13.0-dev-394c0a5")
+    implementation("net.mamoe:mirai-core:2.13.0-dev-394c0a5b")
+    implementation("net.mamoe:mirai-core-api:2.13.0-dev-394c0a5b")
+    implementation("net.mamoe:mirai-core-utils:2.13.0-dev-394c0a5b")
 }
 
 tasks.shadowJar {
