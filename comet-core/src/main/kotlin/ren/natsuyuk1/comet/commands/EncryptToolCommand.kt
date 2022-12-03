e279ddf90af971cf7de75189294a8ad25d7c91cf
StarWishsama <starwishsama@outlook.com>
2022-12-03 09:34:05 +0800

    ✨ feat(encrypt): add `padding` option to aes

--- comet-core/src/main/kotlin/ren/natsuyuk1/comet/commands/EncryptToolCommand.kt
+++ comet-core/src/main/kotlin/ren/natsuyuk1/comet/commands/EncryptToolCommand.kt
@@ -1,11 +1,13 @@
 package ren.natsuyuk1.comet.commands
 
+import cn.hutool.core.codec.Base64
 import cn.hutool.core.util.HexUtil
 import cn.hutool.crypto.Mode
 import cn.hutool.crypto.Padding
 import cn.hutool.crypto.symmetric.AES
 import moe.sdl.yac.core.subcommands
 import moe.sdl.yac.parameters.arguments.argument
+import moe.sdl.yac.parameters.options.default
 import moe.sdl.yac.parameters.options.flag
 import moe.sdl.yac.parameters.options.option
 import moe.sdl.yac.parameters.types.enum
@@ -53,28 +55,36 @@ class EncryptToolCommand(
         private val key by option("--key", "-k", help = "key")
         private val iv by option("--iv", help = "iv")
         private val mode by option("--mode", "-m", help = "模式").enum<Mode>()
+        private val padding by option("--padding", "-p", help = "填充模式").enum<Padding>()
+            .default(Padding.PKCS5Padding)
         private val raw by option("--raw", "-r", help = "原始输出").flag()
+        private val decrypt by option("--decrypt", "-d", help = "使用解密模式").flag()
         private val content by argument("加密内容")
 
         override suspend fun run() {
             if (mode == null) {
-                subject.sendMessage("请提供加密模式!".toMessageWrapper())
+                subject.sendMessage("请提供 AES 模式!".toMessageWrapper())
                 return
             }
 
             val aes = if (key != null && iv != null) {
-                AES(mode, Padding.PKCS5Padding, key!!.toByteArray(), iv!!.toByteArray())
+                AES(mode, padding, key!!.toByteArray(), iv!!.toByteArray())
             } else if (key != null) {
-                AES(mode, Padding.PKCS5Padding, key!!.toByteArray())
+                AES(mode, padding, key!!.toByteArray())
             } else {
                 AES()
             }
 
-            if (raw) {
-                subject.sendMessage(HexUtil.encodeHexStr(aes.encrypt(content)).toMessageWrapper("结果 (hex) > "))
-            } else {
-                subject.sendMessage(aes.encryptBase64(content).toMessageWrapper("结果 (base64) > "))
-            }
+            val result = if (decrypt) aes.decrypt(content) else aes.encrypt(content)
+
+            subject.sendMessage(checkRawMode(result, raw).toMessageWrapper("结果 > "))
         }
     }
 }
+
+private fun checkRawMode(result: ByteArray, raw: Boolean): String =
+    if (raw) {
+        HexUtil.encodeHexStr(result)
+    } else {
+        Base64.encode(result)
+    }
