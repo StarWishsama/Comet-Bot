976f84d1d5e624a6becd3fd3d28101c0390a0f71
StarWishsama <starwishsama@outlook.com>
2022-12-08 19:58:02 +0800

    fix(util): `getEnv` return incorrect env name

--- comet-utils/src/main/kotlin/ren/natsuyuk1/comet/utils/system/Env.kt
+++ comet-utils/src/main/kotlin/ren/natsuyuk1/comet/utils/system/Env.kt
@@ -1,8 +1,10 @@
 package ren.natsuyuk1.comet.utils.system
 
 fun getEnv(propName: String): String? {
-    val env = propName.replace(".".toRegex(), "_").replace("-".toRegex(), "_")
+    val env = propName.replace("\\.".toRegex(), "_").replace("\\-".toRegex(), "_").uppercase()
     return getEnv(propName, env)
 }
 
-fun getEnv(propName: String, env: String): String? = System.getProperty(propName) ?: System.getenv(env)
+fun getEnv(propName: String, env: String): String? =
+    System.getProperty(propName).also { println("prop($propName): $it") } ?: System.getenv(env)
+        .also { println("env($env): $it") }
