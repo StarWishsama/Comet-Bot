d0d8b30161be21c97c7af98b328d61bd3d1b982b
StarWishsama <starwishsama@outlook.com>
2022-12-20 02:12:37 +0800

    fix(brotli): use `mkdir`

--- comet-utils/src/main/kotlin/ren/natsuyuk1/comet/utils/brotli4j/BrotliLoader.kt
+++ comet-utils/src/main/kotlin/ren/natsuyuk1/comet/utils/brotli4j/BrotliLoader.kt
@@ -121,7 +121,7 @@ object BrotliLoader {
 
                 withContext(Dispatchers.IO) {
                     zip.getInputStream(dll).use { input ->
-                        libraryLocation.touch()
+                        libraryLocation.mkdir()
                         libraryLocation.toPath().outputStream().use(input::copyTo)
                     }
                 }
