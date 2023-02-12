72fc4da13ca8d2125ce1743f5ec7aa50988c25c9
StarWishsama <starwishsama@outlook.com>
2023-02-12 13:10:53 +0800

    chore(ci): remove init `CronTaskTable`

--- comet-core/src/test/kotlin/ren/natsuyuk1/comet/test/database.kt
+++ comet-core/src/test/kotlin/ren/natsuyuk1/comet/test/database.kt
@@ -3,10 +3,8 @@ package ren.natsuyuk1.comet.test
 import kotlinx.coroutines.runBlocking
 import ren.natsuyuk1.comet.api.database.DatabaseConfig
 import ren.natsuyuk1.comet.api.database.DatabaseManager
-import ren.natsuyuk1.comet.api.task.CronTasks
 
 fun initTestDatabase() = runBlocking {
     DatabaseConfig.init()
     DatabaseManager.loadDatabase()
-    DatabaseManager.loadTables(CronTasks)
 }
